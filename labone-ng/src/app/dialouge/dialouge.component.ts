81ff6974a1b25cf948406c72a387269b5f45dee7
AngelicMob <angelic97.andersson@gmail.com>
2020-04-01 21:00:50 +0200

    new changes to closer complete

--- labone-ng/src/app/dialouge/dialouge.component.ts
+++ labone-ng/src/app/dialouge/dialouge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { LocalStorageService } from '../local-storage.service';
 
 @Component({
@@ -9,12 +9,60 @@ import { LocalStorageService } from '../local-storage.service';
 })
 export class DialougeComponent implements OnInit {
 
+@ViewChild('inputBoxSection') inputBoxSectionPart: ElementRef;
 
-  constructor(private localstorage: LocalStorageService) { }
+NewTraveler;
+traveler: string;
+travelerSignedUp: boolean = false;
+beverageMeal: string;
+usualBevMealChoice: boolean = false;
+usualBevMeal: string;
+chosen: boolean = false;
+
+  constructor(private renderer: Renderer2, private localstorage: LocalStorageService) { }
 
   ngOnInit(): void {
 
+    this.NewTraveler = this.localstorage.checkIfTravelerExists();
 
+    if (this.NewTraveler === false) {
+
+      this.traveler = this.localstorage.getTravelerName();
+
+    }
+
+}
+
+
+  submit(name: string, lastName: string) {
+
+   if (lastName === '') {
+
+    console.log('Provide a Last name please')
+
+    } else {
+
+    this.localstorage.registerTraveler();
+    this.travelerSignedUp = true;
+    this.inputBoxSectionPart.nativeElement.remove();
+   }
   }
+
+ getBevMeal(beverageMeal: string) {
+
+  this.beverageMeal = this.beverageMeal;
+  this.chosen = true;
+ }
+
+ getUsualOrder(usuals: string) {
+
+  this.usualBevMeal = usuals;
+  this.usualBevMealChoice = true;
+ }
+
+ deleteTraveler() {
+
+  this.localstorage.signOutTraveler();
+}
 
 }
